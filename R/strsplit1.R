d23a8b4f475269fd5dae154079d1d56d9020f07f
Howard Baek <howardhowiebaek@gmail.com>
2022-10-18 19:23:39 -0700

    add documentation and test coverage

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,14 @@
+#' String Manipulation
+#'
+#' @param x vector
+#' @param split object to split
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+#' x <- 'Tall, Short, White, Blue'
+#' strsplit(x, split = ".")
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
